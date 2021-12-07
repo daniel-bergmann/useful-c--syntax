948f7459d1b3ea21dbe014d7bfc1a659a42c1fba
Daniel Bergmann <dba222@outlook.com>
2021-12-07 15:45:52 +0000

    first commit

---
+++ src/HelloWorld.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // int age = 25;
+    // float weight = 65.5;
+    // double balance = 12345.67;
+    // char gender = 'f';
+    // string name = "somename";
+    // bool isTodaySunny = true;
+    // string colors[10] = {"red", "green", "pink", "blue", "black"};
+    // cout << colors[0];
+    // cin >> colors[0];
+    // cout << colors[0];
+    // colors[1] = "gray";
+
+    bool isTodaySunny = true;
+    bool isTodayWeekend = false;
+
+    // if(isTodaySunny && isTodayWeekend) {
+    //     cout<<"Go to the park"<<endl;
+    // }
+    // else if (isTodayWeekend && !isTodaySunny) {
+    //     cout<<"Go to the park but take...."<<endl;
+    // } else {
+
+    //     cout<<"Go to work"<<endl;
+    // }
+
+    // isTodaySunny? cout<<"go to the park" <<endl : cout<<"take umbrella"<<endl;
+
+    // if(isTodaySunny) {
+    //     if(isTodayWeekend) 
+    //         cout<<"Gotothepark"<<endl;
+    //     else 
+    //         cout<<"Go do sth else"<<endl;
+    // }
+    // else 
+    //         cout<<"Go work"<<endl;
+
+
+
+
+    system("pause>0");
+}
